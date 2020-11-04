f5163fde8b15c6aa932f5a1c53243bbed1128181
Robert Jackson <rjackson@linkedin.com>
2020-11-04 15:36:17 -0500

    Use `transitionTo` instead of two way binding of QP

--- app/routes/my-route.js
+++ app/routes/my-route.js
@@ -2,26 +2,25 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   queryParams: {
-    foo: { refreshModel: true }
+    foo: { refreshModel: false }
   },
 
   model(params, transition) {
     let foo = transition.to.queryParams.foo || 'no QPs exist';
-    console.log(`Query Params: ${transition.to.queryParams}`);
+    console.log(`Query Params: ${JSON.stringify(transition.to.queryParams)}`);
 
     return { foo };
   },
 
   actions: {
     addQP() {
-      console.log('updating params');
-      this.set('controller.foo', 'bar');
-      //this.refresh();
+      console.log('updating params NO refresh');
+      this.transitionTo({ queryParams: { foo: 'bar' }});
     },
 
     refresh() {
       console.log('updating params and refreshing');
-      this.set('controller.foo', 'stuff');
+      this.transitionTo({ queryParams: { foo: 'stuff' }});
       this.refresh();
     }
   }
